053f762e1dd764de389deea8cc3d704f0562b424
tuanNH-Vinorsoft <tuannh@vinorsoft.com>
2026-05-08 10:34:35 +0700

    fix lỗi

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,11 @@ import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import {app, server} from "./lib/socket.js";
-
+import path from "path";
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
